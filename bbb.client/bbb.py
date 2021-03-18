d2f532b70c9179c820738a4d480af1d7d4669569
xdBlacky <70610848+xdBlacky@users.noreply.github.com>
2021-03-18 08:52:59 +0100

    Add files via upload

---
+++ bbb.client/bbb.py
@@ -0,0 +1,18 @@
+import selenium 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import tkinter
+from tkinter.constants import *
+from tkinter import *
+from links import *
+
+
+root = tkinter.Tk()
+
+
+#Button_lesson = Button(root, text="lesson", command=lesson, fg="black")
+#Button_lesson.grid(column=1, row=1)
+
+
+root.mainloop()
+
---
+++ bbb.client/links.py
@@ -0,0 +1,35 @@
+import selenium 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import tkinter
+from tkinter.constants import *
+from tkinter import *
+
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+
+#link_lesson = ""
+
+
+#code_lesson = ""
+
+vk = ""
+
+
+#def lesson():
+    #global vk
+    #global link
+    #vk == "lesson"                                            
+    #link = link_lesson     
+    #search_1 = link.replace("https://bbb.stu-vs.de", "")
+    #search_2 = search_1.replace("/", "_")
+    #search = search_2 + "_join_name"
+    #driver.get(link)
+    #print(driver.title)
+    #search = driver.find_element_by_id(search)
+    #search.clear()
+    #search.send_keys(name)                                      
+    #search.send_keys(Keys.RETURN) 
+ 
